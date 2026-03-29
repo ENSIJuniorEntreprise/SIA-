aee62c280abf9767fd06565b7aa416bab364b3b7
islemsaidi703 <islemsaidi703@gmail.com>
2026-03-29 19:49:29 +0100

    Revert "division part1"
    This reverts commit 9f4e0e862200f0217fa11297e77a49d22fae035c.

--- src/pages/travauxpublic.jsx
+++ src/pages/travauxpublic.jsx
@@ -62,7 +62,7 @@ export default function DivisionAuto() {
         <div style={styles.grid}>
           {categories.map((cat) => (
             <Link
-              to={cat.id === 1 ? "/divisions/travaux-publics/moteur-et-groupe-electrogene" : "#"}
+              to={cat.id === 1 ? "/divisions/travaux-publics" : "#"}
               key={cat.id}
               style={{
                 textDecoration: "none",
